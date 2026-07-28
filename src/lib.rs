66eca0cc35d2ed2b89064e7500eb8e15a7cbaefd
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-07-28 16:09:19 +0530

    save and wait

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,14 @@ use app::AppPlugin;
 use bevy::prelude::*;
 use bevy::window::WindowResolution;
 
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub fn run() {
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+
     let primary_window = Window {
         title: "My Ecosystem Bevy".into(),
         resolution: WindowResolution::new(1280, 720),
@@ -26,7 +33,11 @@ pub fn run() {
         .add_plugins(
             DefaultPlugins
                 .set(WindowPlugin {
-                    primary_window: Some(primary_window),
+                    primary_window: Some(Window {
+                        title: "My Ecosystem Bevy".into(),
+                        resolution: (1280, 720).into(),
+                        ..default()
+                    }),
                     ..default()
                 })
                 .set(ImagePlugin::default_nearest()),
