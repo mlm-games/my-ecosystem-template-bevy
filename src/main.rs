6744f1034a755f40f28c04198b1713aa4ecb8592
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-07-28 06:04:04 +0530

    fix transitions

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,3 @@
-mod app;
-mod asset_tracking;
-mod demo;
-mod dev_tools;
-mod ecosystem;
-mod menus;
-mod screens;
-mod theme;
-
-use app::AppPlugin;
-use bevy::prelude::*;
-use bevy::window::WindowResolution;
-
 fn main() {
-    let primary_window = Window {
-        title: "My Ecosystem Bevy".into(),
-        resolution: WindowResolution::new(1280, 720),
-        #[cfg(target_arch = "wasm32")]
-        fit_canvas_to_parent: true,
-        #[cfg(target_arch = "wasm32")]
-        prevent_default_event_handling: true,
-        #[cfg(target_arch = "wasm32")]
-        canvas: Some("#bevy".into()),
-        ..default()
-    };
-
-    App::new()
-        .add_plugins(
-            DefaultPlugins
-                .set(WindowPlugin {
-                    primary_window: Some(primary_window),
-                    ..default()
-                })
-                .set(ImagePlugin::default_nearest()),
-        )
-        .add_plugins(AppPlugin)
-        .run();
+    my_ecosystem_bevy::run();
 }
